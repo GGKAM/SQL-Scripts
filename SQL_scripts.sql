51ad9c1a561a2f86bab9cec3eaa60681c5c601fc
abhiramtilak <46491513+abhiramtilak@users.noreply.github.com>
2020-06-10 23:25:23 +0530

    Created basic table to authenticate login

--- SQL_scripts.sql
+++ SQL_scripts.sql
@@ -1,1 +1,8 @@
+CREATE SCHEMA `assetmaintainance` ;
 
+CREATE TABLE `assetmaintainance`.`users` (
+  `ID` INT NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NULL,
+  `isAdmin` TINYINT NULL,
+  PRIMARY KEY (`email`, `ID`));
